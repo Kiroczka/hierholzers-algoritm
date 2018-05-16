179a61d966d443cbded5ae22244d46a1736ce255
Kira Ivanova <kirroczka@gmail.com>
2018-05-16 17:59:38 +0200

    Implementation created by Przemyslaw Kaleta

---
+++ cmake-build-debug/CMakeFiles/heirholzers_algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/heirholzers_algorithm.dir/main.cpp.obj"
+  "heirholzers_algorithm.pdb"
+  "heirholzers_algorithm.exe"
+  "heirholzers_algorithm.exe.manifest"
+  "heirholzers_algorithm.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/heirholzers_algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
